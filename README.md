08cd988dd7843136429fdf60c1cb7d434664ad7d
panxin <panxin@gmail.com>
2017-10-16 10:48:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## 嘿嘿 LOL
+
+> 自己们瞎jier写写玩 with spring
---
+++ web/src/main/java/hnust/lol405678/WebApplication.java
@@ -0,0 +1,16 @@
+package hnust.lol405678;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author panxin
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
